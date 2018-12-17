58b0c7a3bfd25547e0b6755ba79339cb7295150e
pengpan <i@hvv.me>
2018-12-17 20:05:23 +0800

    Initial commit

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	_ "github.com/pengpan/beego_test/routers"
 	"github.com/astaxie/beego"
+	_ "github.com/pengpan/beego_test/routers"
 )
 
+/*
+You should execute the following script before running.
+
+$ go get -u github.com/astaxie/beego
+$ go get -u github.com/beego/bee
+$ go get -u github.com/smartystreets/goconvey/convey
+
+@see https://beego.me/quickstart
+*/
 func main() {
 	beego.Run()
 }
-
